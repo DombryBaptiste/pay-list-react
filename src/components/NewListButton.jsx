bb2f0246c9a8d26cdbfd300863f0adcf73448bbc
bat dombry <baptiste.dombry12@gmail.com>
2024-11-25 13:55:17 +0100

    Ajout front button nouvelle liste

---
+++ src/components/NewListButton.jsx
@@ -0,0 +1,15 @@
+export function NewListButton() {
+    return (
+        <div className="flex justify-center mt-4">
+            <button
+                className="bg-blue-500 hover:bg-blue-700 font-bold text-white rounded-full py-2 px-4 flex items-center space-x-2">
+                <span>Créer nouvelle liste</span>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                     stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15"/>
+                </svg>
+            </button>
+        </div>
+
+    );
+}
